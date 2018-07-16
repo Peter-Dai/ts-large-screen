1e72e1482b6d072e30a1b7e3c3c6e864d26eec52
daijunyi <daijunyi@chuangjiangx.com>
2018-07-16 14:46:48 +0800

    Initial commit

---
+++ src/views/index/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-container
+    class="container">
+    <el-aside
+      class="left-aside"
+      width="30%">left</el-aside>
+    <el-main
+      class="main"
+      width="40%">Main</el-main>
+    <el-aside
+      class="right-aside"
+      width="30%">right</el-aside>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container{
+  height: 400px;
+}
+
+.left-aside {
+  background-color: red;
+}
+
+.main {
+  background-color: yellow;
+}
+
+.right-aside {
+  background-color: green;
+}
+
+
+</style>
