4a02ebf5d18e171b103c237337b7e64379fa56b6
蒋建雄 <jiangjianxiong@chuangjiangx.com>
2018-07-17 18:13:30 +0800

    柱状图组件开发

--- src/components/common/histogram-bar.vue
+++ src/components/common/histogram-bar.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>histogram-bar</div>
+  <div
+    ref="histogramBar"
+    class="histogramBar">
+    histogramBar
+  </div>
 </template>
 
 <script>
@@ -10,10 +14,89 @@ export default {
       msg: '',
     };
   },
+  mounted() {
+    const myChart = this.$echarts.init(this.$refs.histogramBar);
+
+    const option = {
+      title: {
+        text: '最近30天交易额 TOP 5 城市',
+        textStyle: {
+          color: '#fff',
+        },
+      },
+      grid: [{
+        width: '100%',
+        height: '80%',
+        x: 5,
+        y: 35,
+        containLabel: true,
+      }],
+      tooltip: {
+        trigger: 'axis',
+      },
+      xAxis: [
+        {
+          show: false,
+        },
+      ],
+      yAxis: [
+        {
+          type: 'category',
+          axisLabel: { // 坐标轴文本标签选项
+            textStyle: {
+              color: '#fff',
+              fontSize: 12,
+            },
+          },
+          splitLine: { // 分隔线
+            show: false,
+          },
+          axisTick: { // 坐标刻度线
+            show: false,
+          },
+          axisLine: { // 坐标轴线
+            show: false,
+          },
+          data: ['深圳', '北京', '上海', '南京', '杭州'],
+        },
+      ],
+      series: [
+        {
+          name: '最近30天交易额',
+          type: 'bar',
+          label: {
+            normal: {
+              position: 'right',
+              show: true,
+              color: '#fff',
+            },
+          },
+          itemStyle: {
+            normal: {
+              color: '#0f35ff',
+            },
+          },
+          barWidth: 5,
+          data: [83.21, 93.21, 103.21, 113.21, 123.21],
+        },
+      ],
+    };
+    myChart.setOption(option);
+
+    // window.onresize = () => {
+    //   // 重置容器高宽
+    //   resizeWorldMapContainer();
+    //   myChart.resize();
+    // };
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.histogramBar {
+  width: 100%;
+  height: 100%;
+  line-height: 100%;
+}
 </style>
--- src/components/index/top-five-summary.vue
+++ src/components/index/top-five-summary.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>top-five-summary</div>
+  <el-container
+    class="top-container">
+    <el-header
+      height="35%"
+      class="top-header">
+      <histogramBar/>
+    </el-header>
+    <!--<el-main>main</el-main>-->
+    <!--<el-footer style="height: 30%">footer</el-footer>-->
+  </el-container>
 </template>
 
 <script>
+import HistogramBar from '../common/histogram-bar';
+
 export default {
   name: 'TopFiveSummary',
+  components: { HistogramBar },
   data() {
     return {
       msg: '',
@@ -15,5 +27,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.top-container {
+  height: 100%;
+}
 
+.top-header {
+  margin-top: 10px;
+  background: #081249;
+}
 </style>
