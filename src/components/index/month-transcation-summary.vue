e50764424fcf4696b929a3b9e28929405d81ed1b
daijunyi <daijunyi@chuangjiangx.com>
2018-07-19 10:45:08 +0800

    搭建月度汇总结构

--- src/components/index/month-transcation-summary.vue
+++ src/components/index/month-transcation-summary.vue
@@ -1,8 +1,31 @@
 <template>
-  <div>month-transcation-summary</div>
+  <el-container
+    class="top-container">
+    <div style="text-align:left">
+      最近30天实时数据
+
+    </div>
+    <el-header
+      height="22%"
+      class="top-header">
+      <histogramBar/>
+    </el-header>
+    <el-header
+      height="35%"
+      class="top-header">
+      <AreaLine/>
+    </el-header>
+    <el-header
+      height="35%"
+      class="top-header">
+      <AreaLine/>
+    </el-header>
+  </el-container>
 </template>
 
 <script>
+import AreaLine from '../common/area-line';
+
 export default {
   name: 'MonthTranscationSummary',
   data() {
@@ -10,10 +33,27 @@ export default {
       msg: '',
     };
   },
+  components: {
+    AreaLine,
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.top-container {
+  height: 100%;
+  /* padding-left: 6%;
+  padding-top: 10px; */
+  padding: 10px;
+}
 
+.top-header {
+  margin-bottom: 10px;
+  background: #081249;
+}
+
+.top-header:last-child {
+  margin-bottom: 0;
+}
 </style>
